a967b8faad23fa6f53c566e6ca756576fa9f3a15
Tamires Cordeiro <mirescordeiro@gmail.com>
2025-02-28 22:33:01 -0300

    learn(ap04-react): encerra o codigo 2.5.4

---
+++ componentes-props/src/App.jsx
@@ -0,0 +1,25 @@
+const App = () => {
+  return (
+    <div className='container border rounded mt-2'>
+      <div className='row'>
+        <div className='col-sm-12 col-lg-6 col-xxl-3'>
+          <p>caixa 1</p>
+        </div>
+
+        <div className='col-sm-12 col-lg-6 col-xxl-3'>
+          <p>caixa 2</p>
+        </div>
+
+        <div className='col-sm-12 col-lg-6 col-xxl-3'>
+          <p>caixa 3</p>
+        </div>
+
+        <div className='col-sm-12 col-lg-6 col-xxl-3'>
+          <p>caixa 4</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App;
